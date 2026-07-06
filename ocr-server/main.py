ef4b92a3cf3c6db8f36eb5625ac16bde43bfb2e3
Bruno Costa Prado <brunocostaprado@gmail.com>
2026-07-05 23:59:36 -0300

    edit docker

--- ocr-server/main.py
+++ ocr-server/main.py
@@ -270,4 +270,4 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.environ.get("OCR_PORT", "8000"))
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
